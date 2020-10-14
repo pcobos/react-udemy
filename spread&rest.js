cadbd22fa54b8f112e02336336faffba0e4ec31d
Pedro Cobos <pedro.cobos@live.com>
2020-10-13 22:40:28 -0500

    spread operator example

---
+++ spread&rest.js
@@ -0,0 +1,6 @@
+// Spread operator (...)
+const numbers = [1, 2, 3, 4];
+const newNumbers = [...numbers, 5];
+// similar to push(), but instead, we copy the elements of an existing array into a new one.
+
+console.log(newNumbers);
