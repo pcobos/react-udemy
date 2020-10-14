fecb215a910319246b90c1fb72db522dc522fc3d
Pedro Cobos <pedro.cobos@live.com>
2020-10-13 22:44:30 -0500

    spread operator example for objects

--- spread&rest.js
+++ spread&rest.js
@@ -1,6 +1,18 @@
-// Spread operator (...)
+// Spread operator for arrays (...)
+// similar to push(), but instead, we copy the elements of an existing array into a new one.
 const numbers = [1, 2, 3, 4];
 const newNumbers = [...numbers, 5];
-// similar to push(), but instead, we copy the elements of an existing array into a new one.
-
 console.log(newNumbers);
+
+// Spread operator for objects (...)
+// Basically, we are copying the properties of an existing object into another one
+const person = {
+  name: "Pedro"
+};
+
+const newPerson = {
+  ...person,
+  age: 28
+}
+
+console.log(newPerson);
